8ae708b1bf530700db4302173e7431183de59772
Brandon <railsdev14@gmail.com>
2015-12-01 14:22:30 -0500

    added save and delete functionality
    added save and delete functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,42 @@
 angular.module("todoListApp", [])
-  .controller('mainCtrl', function($scope) {
-    $scope.helloWorld = function() {
-      console.log("Hello there");
+  .controller('mainCtrl', function($scope, dataService) {
+    $scope.addTodo = function() {
+      var todo = {name: "This is a new todo."}
+      $scope.todo.push(todo);
+    }
+  })
+
+    $scope.helloWorld = dataService.helloWorld;
+
+    dataService.getTodos(function(response) {
+      console.log(response.data);
+      $scope.todos = response.data;
+    });
+
+    $scope.deleteTodo = function(todo, $index) {
+      dataService.deleteTodo(todo);
+      $scope.todo.splice($index, 1);
+    }
+
+    $scope.saveTodo = function(todo, $index) {
+      dataService.saveTodo(todo);
+    }
+
+  .service('dataService', function($http) {
+    this.helloConsole = function() {
+      console.log("This is a service");
     };
+
+    this.getTodos = function(callback) {
+      $http.get('mock/todos.json')
+      .then(callback)
+    };
+
+    this.deleteTodo = function(todo, $index) {
+      console.log("The " + todo.name + " todo has been deleted");
+    };
+
+    this.saveTodos = function(todo) {
+      console.log("The " + todo.name + " todo has been saved");
+    }
   });
